40603dfedc950e0d3412e7df180b3f2bc85d9572
keih93 <nnquang7114@gmail.com>
2019-07-16 00:30:47 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
- 􀀀
- #include <omp.h>
- #include <stdio.h>
- #include <stdlib.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-int i = 10;
-int sum;
-#pragma omp parallel private(i) reduction( +:sum)
-{
-printf("thread %d: i = %d\n", omp_get_thread_num(), i);
-i = 1000 + omp_get_thread_num();
-sum = omp_get_thread_num();
+  int i = 10;
+  int sum;
+  #pragma omp parallel private(i) reduction( +:sum)
+  {
+    printf("thread %d: i = %d\n", omp_get_thread_num(), i);
+    i = 1000 + omp_get_thread_num();
+    sum = omp_get_thread_num();
+  }
+  printf("i = %d, sum = %d\n", i, sum);
 }
-printf("i = %d, sum = %d\n", i, sum);
