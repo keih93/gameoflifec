48856819a3741ffc542dd60d20896f17906360e0
keih93 <nnquang7114@gmail.com>
2019-07-16 02:56:08 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <omp.h>
+int main() {
+#pragma omp parallel num_threads(4)
+{
+#pragma omp single
+// Only a single thread can read the input .
+printf("read input\n");
 
- int main() {
-#pragma omp parallel sections num_threads(4)
- {
- printf("1 Hello from thread %d\n", omp_get_thread_num());
- #pragma omp section
- printf("2 Hello from thread %d\n", omp_get_thread_num());
- #pragma omp section
- printf("3 Hello from thread %d\n", omp_get_thread_num());
+ // Multiple threads in the team compute the results .
+ printf("compute results\n");
+
+ #pragma omp single
+ // Only a single thread can write the output .
+ printf("write output\n");
  }
- return 0;
- }
+ } 
